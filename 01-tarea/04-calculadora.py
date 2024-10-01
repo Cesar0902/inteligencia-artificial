7c5115273bc2d6603b4819e5f945c2f6eb744caf
Cesar0902 <cescobarp@unah.hn>
2024-10-01 06:21:16 -0600

    Ejercicio 4 - Calculadora

---
+++ 01-tarea/04-calculadora.py
@@ -0,0 +1,26 @@
+class Calculadora:
+  def __init__(self, num1, num2):
+    self.num1 = num1
+    self.num2 = num2
+  
+  def sumar(self):
+    return self.num1 + self.num2
+
+  def restar(self):
+    return self.num1 - self.num2
+
+  def multiplicar(self):
+    return self.num1 * self.num2
+  
+  def dividir(self):
+    return self.num1 / self.num2
+  
+
+print("Calculadora")
+num1, num2 = map(int, input("Ingrese dos números separados por espacio: ").split())
+calculadora = Calculadora(num1, num2)
+
+print(f"  {num1} + {num2} = {calculadora.sumar()}")
+print(f"  {num1} - {num2} = {calculadora.restar()}")
+print(f"  {num1} * {num2} = {calculadora.multiplicar()}")
+print(f"  {num1} / {num2} = {calculadora.dividir()}")
