bb0063421dfbf04d0254d8ef19e99599ea7464bc
Cesar0902 <cescobarp@unah.hn>
2024-10-02 07:05:25 -0600

    Ejercicio 10: Creando la funcion generador_contraseñas solicitando al usuario la longitud

--- 01-tarea/10-generador-contraseñas.py
+++ 01-tarea/10-generador-contraseñas.py
@@ -1,20 +1,28 @@
 from random import choice, randint, shuffle
 
-patron = ['M', 'm', 'n', 's']
-contraseña = ""
+def generar_contraseña(longitud):
+  patron = ['M', 'm', 'n', 's']
+  contraseña = ""
 
-patron += [choice(patron) for _ in range(4)]
-shuffle(patron)
+  patron += [choice(patron) for _ in range(longitud - 4)]
+  shuffle(patron)
 
-for letra in patron:
-  match letra:
-    case 'M':
-      contraseña += chr(randint(65, 90))
-    case 'm':
-      contraseña += chr(randint(97, 122))
-    case 'n':
-      contraseña += chr(randint(48, 57))
-    case 's':
-      contraseña += chr(choice([randint(33, 47), randint(58, 64), randint(91, 96)]))
+  for letra in patron:
+    match letra:
+      case 'M':
+        contraseña += chr(randint(65, 90))
+      case 'm':
+        contraseña += chr(randint(97, 122))
+      case 'n':
+        contraseña += chr(randint(48, 57))
+      case 's':
+        contraseña += chr(choice([randint(33, 47), randint(58, 64), randint(91, 96)]))
 
-print(f"Contraseña generada es: {contraseña}")
+  print(f"Contraseña generada es: {contraseña}")
+
+while True:
+  longitud = int(input("Ingrese la longitud de la contraseña (minimo 8): "))
+  if longitud >= 8:
+    break
+
+generar_contraseña(longitud)
