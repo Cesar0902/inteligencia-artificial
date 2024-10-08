436a61d471a95e29bdea2ca6c6ab6d89ad1ab392
Cesar0902 <cescobarp@unah.hn>
2024-10-08 14:53:55 -0600

    Validando que la entrada sea un entero positivo

--- 06-fibonacci.py
+++ 06-fibonacci.py
@@ -6,10 +6,18 @@ def fibonacci(n):
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
-n = int(input("Ingrese el límite de la serie de Fibonacci: "))
+while True:
+    try:
+        n = int(input("Ingrese el límite de la serie de Fibonacci: "))
+        if n < 0:
+            print("El número debe ser positivo")
+        else:
+            break
+    except ValueError:
+        print("Ingrese un número entero")
 
 for i in range(n):
     if i < n-1:
-      print(fibonacci(i), end=", ")
+        print(fibonacci(i), end=", ")
     else:
-      print(fibonacci(i), end="\n")
+        print(fibonacci(i), end="\n")
