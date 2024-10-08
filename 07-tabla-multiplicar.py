196044845158da4a4b80688281d75603033e9c00
Cesar0902 <cescobarp@unah.hn>
2024-10-08 14:54:25 -0600

    Validando que la entrada sea un entero

--- 07-tabla-multiplicar.py
+++ 07-tabla-multiplicar.py
@@ -1,4 +1,9 @@
-numero = int(input("Ingrese un número: "))
+while True:
+    try:
+        numero = int(input("Ingrese un número: "))
+        break
+    except ValueError:
+        print("Ingrese un número entero")
 
 for i in range(1, 11):
     print(f"{numero} x {i} = {numero * i}")
